a2c28325b6fdee77351ae8cd2cb8d716ad57d316
Ozan <ozankutlar@protonmail.com>
2024-11-30 11:31:58 +0300

    Added omnidirectional movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		jumpTimer = jumpTimer + 1
-		velocity.y += gravity * delta
+		#velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and (jumpTimer < 10):
@@ -23,6 +23,7 @@ func _physics_process(delta):
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
+	var directionY = Input.get_axis("move_up", "move_down")
 	
 	# Flip the Sprite
 	if direction > 0:
@@ -30,20 +31,14 @@ func _physics_process(delta):
 	elif direction < 0:
 		animated_sprite.flip_h = true
 	
-	# Play animations
-	if is_on_floor():
-		jumpTimer = 0
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-	
-	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if directionY:
+		velocity.y = directionY * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
