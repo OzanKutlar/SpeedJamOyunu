ddfb11f4e0697e5b40096e8a92bf2d0d71032fae
Ozan <ozankutlar@protonmail.com>
2024-11-30 12:42:42 +0300

    Added wavecurrent

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,11 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var jumpTimer = 0
 
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		jumpTimer = jumpTimer + 1
-		#velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and (jumpTimer < 10):
-		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -34,11 +23,11 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = velocity.x * 0.6
 		
 	if directionY:
 		velocity.y = directionY * SPEED
 	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.y = velocity.y * 0.6
 
 	move_and_slide()
