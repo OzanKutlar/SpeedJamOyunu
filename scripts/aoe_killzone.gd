0d89ef6a25037863932dd81d3da18d37dd2f7f02
Ozan <ozankutlar@protonmail.com>
2024-12-01 09:57:09 +0300

    Moved some scripts to dedicated folder

--- scripts/aoe_killzone.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body):
-		print("You died!")
-		body.position = body.initial_position
-		
--- scripts/killzone.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-@onready var timer = $Timer
-
-
-func _on_body_entered(body):
-	print("You died!")
-	body.position = body.initial_position
