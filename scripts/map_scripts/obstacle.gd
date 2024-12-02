9e7e0163eca693c9340614b5f3c234e406ee01a1
Ozan <ozan@localhost.localdomain>
2024-12-02 12:24:37 +0300

    Added shoe spawner

--- scripts/map_scripts/obstacle.gd
+++ scripts/map_scripts/obstacle.gd
@@ -7,6 +7,6 @@ var externalForce = Vector2(0,0)
 @onready var collider: CollisionShape2D = $CollisionShape2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	collider.disabled = player.sizeProgress >= 0.2
 	self.position += externalForce * player.SPEED
